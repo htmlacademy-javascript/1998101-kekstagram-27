80aebd865ca6ec56385aa3d0d8d582ecfde2d6f7
litos2 <97879932+litos2@users.noreply.github.com>
2022-11-10 23:09:42 +0700

    Добавляет исправления замечаний и доработки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 import {createObjects} from './data.js';
 import {createMiniatures} from './miniatures.js';
-import {showModal} from './setupModal.js';
+import {setupModal} from './setupModal.js';
 import {showFormWithValidation} from './form.js';
+import {initScale} from './scaleImages.js';
+// import {initFilters} from './effectsImages.js';
 
+
+initScale();
+// initFilters();
 showFormWithValidation();
 
 const pictures = createObjects(25);
-createMiniatures(pictures, showModal);
+createMiniatures(pictures, setupModal);
