1eed9eb40a3581916e49e2442667331182f1c76a
Keks <keks@htmlacademy.ru>
2022-11-13 17:27:20 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/effectsImages.js
@@ -0,0 +1,128 @@
+const effectSliderContainer = document.querySelector('.img-upload__effect-level');
+const effectValue = document.querySelector('.effect-level__value');
+const uploadPhoto = document.querySelector('.img-upload__preview');
+let effectCurrentClass;
+const effectOriginal = document.querySelector('#effect-none');
+const effectChrome = document.querySelector('#effect-chrome');
+const effectSepia = document.querySelector('#effect-sepia');
+const effectMarvin = document.querySelector('#effect-marvin');
+const effectPhobos = document.querySelector('#effect-phobos');
+const effectHeat = document.querySelector('#effect-heat');
+let isSliderInitialized = false;
+
+const initFilters = () => {
+
+  // По умолчанию выбран эффект «Оригинал».
+  effectOriginal.addEventListener('change', () => {
+    if (effectOriginal.checked) {
+      uploadPhoto.classList.remove(effectCurrentClass);
+      uploadPhoto.style.filter = '';
+      if (isSliderInitialized) {
+        effectSliderContainer.noUiSlider.destroy();
+        isSliderInitialized = false;
+      }
+    }
+  });
+
+  const activateFilter = (filterEffectClass, getFilterStyle) => {
+    if (!isSliderInitialized) {
+      noUiSlider.create(effectSliderContainer, {
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.01,
+        connect: 'lower',
+      });
+      isSliderInitialized = true;
+    }
+    uploadPhoto.classList.remove(effectCurrentClass);
+    effectSliderContainer.noUiSlider.off();
+    uploadPhoto.classList.add(filterEffectClass);
+    effectCurrentClass = filterEffectClass;
+    effectSliderContainer.noUiSlider.on('update', () => {
+      effectValue.value = effectSliderContainer.noUiSlider.get();
+      uploadPhoto.style.filter = getFilterStyle(effectValue.value);
+    });
+  };
+
+  // Эффект «Хром»
+  effectChrome.addEventListener('change', (evt) => {
+    if (evt.target.checked) {
+      activateFilter('effects__preview--chrome', (currentValue) => `greyscale(${currentValue})`);
+
+      effectSliderContainer.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1,
+      });
+    }
+  });
+
+  // Эффект «Сепия»
+  effectSepia.addEventListener('change', (evt) => {
+    if (evt.target.checked) {
+      activateFilter('effects__preview--sepia', (currentValue) => `sepia(${currentValue})`);
+
+      effectSliderContainer.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1,
+      });
+    }
+  });
+
+  // Эффект «Марвин»
+  effectMarvin.addEventListener('change', (evt) => {
+    if (evt.target.checked) {
+      activateFilter('effects__preview--marvin', (currentValue) => `invert(${currentValue}%)`);
+      effectSliderContainer.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: 100,
+        step: 1,
+      });
+    }
+  });
+
+  // Эффект «Фобос»
+  effectPhobos.addEventListener('change', (evt) => {
+    if (evt.target.checked) {
+      activateFilter('effects__preview--phobos', (currentValue) => `blur(${currentValue}px)`);
+      effectSliderContainer.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1,
+      });
+    }
+  });
+
+  // Эффект «Зной»
+  effectHeat.addEventListener('change', (evt) => {
+    if (evt.target.checked) {
+      activateFilter('effects__preview--heat', (currentValue) => `brightness(${currentValue})`);
+      effectSliderContainer.noUiSlider.updateOptions({
+        range: {
+          min: 1,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1,
+      });
+    }
+  });
+};
+
+export {initFilters};
---
+++ 9/js/setupModal.js
@@ -0,0 +1,84 @@
+import './miniatures.js';
+
+const bigPicture = document.querySelector('.big-picture');
+const templateComment = document.querySelector('#comment').content;
+const closeModalButton = bigPicture.querySelector('.big-picture__cancel');
+const loadMoreButton = document.querySelector('.social__comments-loader');
+const socialComments = bigPicture.querySelector('.social__comments');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const body = document.body;
+
+const setupComments = (comments) => {
+  let count = 5;
+  const visibleComments = [];
+
+  const checkCommentsQuantity = () => comments.length > 5;
+
+  const renderComments = (commentsArr) => {
+    const commentsContainer = document.createDocumentFragment();
+
+    commentsArr.forEach(({avatar, message, name}) => {
+      const comment = templateComment.cloneNode(true);
+      comment.querySelector('.social__picture').src = avatar;
+      comment.querySelector('.social__text').textContent = message;
+      comment.querySelector('.social__picture').alt = name;
+      commentsContainer.appendChild(comment);
+    });
+    socialComments.appendChild(commentsContainer);
+  };
+  if (checkCommentsQuantity(comments)) {
+    loadMoreButton.classList.remove('hidden');
+    const commentsToShow = comments.slice(0, 5);
+    visibleComments.push(...commentsToShow);
+    renderComments(commentsToShow);
+    loadMoreButton.addEventListener('click', () => {
+      const commentsArr = comments.slice(count, count + 5);
+      count += 5;
+      renderComments(commentsArr);
+      visibleComments.push(...commentsArr);
+      if (comments.length === visibleComments.length) {
+        loadMoreButton.classList.add('hidden');
+      }
+    }, { once: true });
+  } else {
+    loadMoreButton.classList.add('hidden');
+    renderComments(comments);
+  }
+};
+
+//Функция закрытия окна по Escape
+const onEscapeKeydown = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+  }
+};
+
+// Закрытие модального окна
+const closeModal = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  closeModalButton.removeEventListener('click', closeModal);
+  document.removeEventListener('keydown', onEscapeKeydown);
+};
+
+// Открытие модального окна
+const showModal = () => {
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+  closeModalButton.addEventListener('click', closeModal);
+  document.addEventListener('keydown', onEscapeKeydown);
+};
+
+const setupModal = (url, likes, comments, description) => {
+  socialComments.innerHTML = '';
+  commentsCount.textContent = comments.length;
+  bigPicture.querySelector('.big-picture__img img').src = url;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.social__caption').textContent = description;
+  setupComments(comments);
+
+  showModal();
+};
+
+export{setupModal};
